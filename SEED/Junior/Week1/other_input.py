53efd1122d36d2cb0a686ecde60144243f32f8dd
jm-kor-00 <tomy000120@naver.com>
2023-02-16 00:39:42 +0900

    230216

---
+++ SEED/Junior/Week1/other_input.py
@@ -0,0 +1,14 @@
+# year, month, day = map(int,input("년도,월,일을 입력 : ").split())
+# print(year,"년",month,"월",day,"일")
+
+# name1, name2, name3 = map(str,input("최애 아이돌 3명 입력 : ").split())
+# print(name1,name2,name3)
+
+# uni1, uni2, uni3 = map(str,input("대학교 이름을 쉼표로 구분해서 입력 : ").split(','))
+# print(uni1,uni2,uni3)
+
+numbers = list(map(int,input().split()))
+print(numbers)
+
+strings = list(map(str,input().split()))
+print(strings)
--- SEED/Junior/Week1/simple_input.py
+++ SEED/Junior/Week1/simple_input.py
@@ -1,8 +1,8 @@
-String = input()
+String = input("문자열 입력 : ")
 print(String)
 
-Integer = int(input())
+Integer = int(input("정수 입력 : "))
 print(Integer)
 
-Float = float(input())
+Float = float(input("실수 입력 : "))
 print(Float)
