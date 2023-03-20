ce8af5ad01d9bade3cee49d090ef88b9b939ab60
jm-kor-00 <tomy000120@naver.com>
2023-03-21 00:51:49 +0900

    230321

--- SEED/Junior/Week2/forRoopPrac.py
+++ SEED/Junior/Week2/forRoopPrac.py
@@ -1,21 +1,64 @@
 N = 10
-#0부터 N-1까지 반복
-for i in range(0,N):
-    print(i)
-#증가폭 수정 가능!
-for i in range(N,0,-1):
-    print(i)
-for i in range(-1,N,3):
-    print(i)
-#특정값안에서 반복
-for i in (1,3,7,8):
-    print(i)
+# range란?
+# range(N) = 0, 1, 2,..., N - 1
 
-#for-each문
+# 디폴트 : range(0,N,1)
+
+# range(start,end,increase)는 
+# start부터 end-1까지 increase간격으로 있는 모든 정수를 반환하는 함수
+
+#0부터 10까지, 간격 : 1
+# arr = list(range(10))
+# print("arr =",arr)
+
+#10부터 0까지, 간격 : -1
+# brr = list(range(10,0,-1))
+# print("brr =",brr)
+
+#0부터 10까지, 간격 : 2
+# crr = list(range(0,10,2))
+# print("crr =",crr)
+
+#for문의 기본 형태
 #리스트의 요소들로 반복:
-arr = ['a','b','c','d']
-for el in arr:
-    print(el)
-#증감자를 사용할 필요가 없을 때
-for _ in range(0,5):
-    print("###")
+
+# print("[arr]")
+# for i in arr:
+#     print(i)
+
+# arr = ['a','b','c','d']
+# print("arr =",arr)
+# for el in arr:
+#     print(el)
+
+#for문과 range()의 혼용
+# print("[0부터 N까지]")
+# for i in range(N):
+#     print(i)
+
+# print("[N부터 0까지]")
+# for i in range(N,0,-1):
+#     print(i)
+
+# print("[0부터 N까지, 3간격]")
+# for i in range(0,N,3):
+#     print(i)
+
+#특정값안에서 반복
+# print("[100,230,410,55을 순환]")
+# for i in (100,230,410,55):
+#     print(i)
+
+
+#증감변수가 필요없을 때
+# for _ in range(0,5):
+#     print("###")
+
+#i는 반복문 안에서만 사용한다!
+
+# for i in range(5):
+#     print(i)
+# print("i =",i)
+
+#근데 이거 왜 돼냐... 파이썬은 신인가? => X
+#쓰지말아요 우리. 제발
--- SEED/Junior/Week2/gugu.py
+++ SEED/Junior/Week2/gugu.py
@@ -1,3 +1,3 @@
-for i in range(1,4):
+for i in range(1,3):
     for j in range(1,10):
         print(i,'X',j,"=",i * j)
